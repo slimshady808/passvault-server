0308804540ad1be29ee27197dd4ff37e430a17c2
akshay <akshay@gmail.com>
2023-09-29 14:41:56 +0530

    delete password

--- password/urls.py
+++ password/urls.py
@@ -3,4 +3,7 @@ from .import views
 
 urlpatterns = [
     path('', views.getRoutes),
+    path('password/',views.PasswordCreateListView.as_view(),name='password-create-list'),
+    path('password/<int:user_id>/',views.PasswordListByUserIDView.as_view(),name='password-list'),
+    path('delete_password/<int:id>/',views.delete_password,name='delete-password')
 ]
--- password/views.py
+++ password/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView,ListAPIView
 from rest_framework.response import Response
+from .serializers import PasswordSerializer
+from .models import Password
 # Create your views here.
 
 @api_view(['GET'])
@@ -14,3 +16,25 @@ def getRoutes(request):
         
     ]
     return Response(routes)
+
+class PasswordCreateListView(CreateAPIView):
+    queryset=Password.objects.all()
+    serializer_class = PasswordSerializer
+
+class PasswordListByUserIDView(ListAPIView):
+    serializer_class = PasswordSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']  # Assuming 'user_id' is the URL parameter name
+        queryset = Password.objects.filter(user=user_id)  # Adjust the field name as per your model
+        return queryset
+    
+@api_view(['DELETE'])
+def delete_password(request, id):
+    try:
+        password=Password.objects.get(id=id)
+    except Password.DoesNotExist:
+        return Response({'message': 'password not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    password.delete()
+    return Response({'message': 'password deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
